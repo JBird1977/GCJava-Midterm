6303d9bd63ac8db38a4dd779458ec422a6d6f600
George Icsman <sgicsman@gmail.com>
2019-02-01 15:08:16 -0500

    george 2

--- src/MidtermApp.java
+++ src/MidtermApp.java
@@ -5,7 +5,9 @@ public class MidtermApp {
 
 
     	System.out.println("George is here!");
-    	System.out.println("Jason is also here!");
+    	System.out.println("Jason is also here!");  
+    	
+    	System.out.println("George #2 test");
     }
 
 }
