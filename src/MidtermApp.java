4ef477a6e69bbc2c35883a477918233454a74981
George Icsman <sgicsman@gmail.com>
2019-02-04 12:06:44 -0500

    added methods for taking user #mines and placing mines

--- src/MidtermApp.java
+++ src/MidtermApp.java
@@ -1,10 +1,18 @@
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MidtermApp {
 
 
+   	// CREATE 2D ARRAY FOR MINEFIELD
+	// user enters 2 integers -- i for the # of columns, n for the # of mines
+	// iterate n times through a for loop using random.int to generate both coordinates (y,x) for the locations of mines
+	// use an if-then to check for duplicate locations -- when found, i-- will force an extra iteration
+	
+
+	
     public static void main(String[] args) 
     {
         Scanner scan = new Scanner(System.in);
@@ -15,7 +23,14 @@ public class MidtermApp {
         printTitle();   //prints title
         do {y = getYCoordinate(scan);} while ((y == -1));
         do {x = getXCoordinate(scan);} while ((x == -1));
-        createBoard(x, y, gameBoard);         
+        createBoard(x, y, gameBoard);  
+        
+        // take user input for # of mines
+        int numMines = getNumMines(scan);
+        
+        // place mines randomly
+        placeMines(x, y, numMines, gameBoard);
+        
     } // end Main
 
     private static void printTitle()
@@ -82,4 +97,44 @@ public class MidtermApp {
         
     }
     
+    
+    private static int getNumMines(Scanner scan) 
+    {
+        int in = -1;
+        System.out.println("Please enter how many mines you would like in your minefield: ");
+        try 
+        {
+            in = scan.nextInt();
+        } catch (InputMismatchException e) 
+        {
+            System.out.println("Please enter a number.");
+            return -1;
+        }
+        return in;
+    } // end getnumMines
+    
+    
+    private static void placeMines(int x, int y, int numMines, ArrayList<ArrayList<Minefield>> gameBoard) 
+    {
+    	int numMinesSet = 0;
+    	Random rand = new Random();
+    	int randomYCoordinate = 0;
+    	int randomXCoordinate = 0;
+    	
+    	while (numMinesSet <= numMines) {
+         
+        // get random coordinates to place a mine 
+    	randomYCoordinate = rand.nextInt(y + 1);
+    	randomXCoordinate = rand.nextInt(x + 1);
+    	
+    		if (! ( gameBoard.get(y).get(x).getMine() ) ) {
+    
+    			gameBoard.get(y).get(x).setMine(true);
+    			numMinesSet ++;		
+    		}
+    	}
+    	
+    	
+
+    }
 }
